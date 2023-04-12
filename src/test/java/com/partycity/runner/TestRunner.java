e864a2720898a226b003a61431e9dc64baa3c198
shakthinakkeeran7 <shakthinakkeeran@gmail.com>
2023-04-12 16:20:45 +0530

    update

--- src/test/java/com/partycity/runner/TestRunner.java
+++ src/test/java/com/partycity/runner/TestRunner.java
@@ -1,6 +1,5 @@
 package com.partycity.runner;
 
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
@@ -28,56 +27,31 @@ import io.cucumber.testng.PickleWrapper;
 import io.cucumber.testng.TestNGCucumberRunner;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
+@CucumberOptions(features = "src/test/resources/feature/account-login.feature", glue = {
+		"com.partycity.stepDefinition" }, dryRun = true,  monochrome = true)
 
+public class TestRunner extends AbstractTestNGCucumberTests {
+	// Parallel Execution Scenario
 
-@CucumberOptions(
-        features = "src/test/resources/feature/account-login.feature",
-        glue = {"com.partycity.stepDefinition"},
-        dryRun = true,
-        plugin = {"json:target/cucumber-reports/Cucumber.json", "html:target/cucumber-reports"},
-        monochrome = true		)
+	private TestNGCucumberRunner testNGCucumberRunner;
+	private static ExtentHtmlReporter htmlReporter;
+	private static ExtentReports extent;
+	private static ExtentTest test;
 
-public class TestRunner	extends AbstractTestNGCucumberTests {
-	    //Parallel Execution Scenario
-	   
-    private TestNGCucumberRunner testNGCucumberRunner;
-    private static ExtentHtmlReporter htmlReporter;
-    private static ExtentReports extent;
-    private static ExtentTest test;
-	
-	    public static WebDriver driver;
+	public static WebDriver driver;
 
-	    
-	    @BeforeSuite(alwaysRun = true)
-	    public void setUp() throws Throwable {
-	    	
-	    	
-	        // Set up ExtentReports
-	        htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") + "/test-output/ExtentReport.html");
-	        htmlReporter.config().setDocumentTitle("Test Automation Report");
-	        htmlReporter.config().setReportName("Functional Test Report");
-	        htmlReporter.config().setTheme(Theme.STANDARD);
-	        extent = new ExtentReports();
-	        extent.attachReporter(htmlReporter);
-	        extent.setSystemInfo("Host Name", "localhost");
-	        extent.setSystemInfo("Environment", "Test Environment");
-	        extent.setSystemInfo("User Name", "Test User");
-	       
-			String browser = FileReaderManager.getInstance().getCrInstance().getBrowser();
-			driver = BaseClass.getBrowser(browser);
-			
-	    
-	    }
-	    
-	   
-	
-		@Override
-	    @DataProvider(parallel = true)
-	    public Object[][] scenarios() {
-	        return super.scenarios();
-	    }
-		
-		
-		
-			
+	@BeforeSuite(alwaysRun = true)
+	public void setUp() throws Throwable {
+
+		String browser = FileReaderManager.getInstance().getCrInstance().getBrowser();
+		driver = BaseClass.getBrowser(browser);
+
+	}
+
+	@Override
+	@DataProvider(parallel = true)
+	public Object[][] scenarios() {
+		return super.scenarios();
+	}
+
 }
--- src/test/java/com/partycity/stepDefinition/LoginPageTestSteps.java
+++ src/test/java/com/partycity/stepDefinition/LoginPageTestSteps.java
@@ -18,15 +18,86 @@ public class LoginPageTestSteps extends BaseClass {
 	LoginPage loginPage;
 
 	public LoginPageTestSteps(TestContext context) {
-		
-		
+
 		testContext = context;
 		loginPage = testContext.getPageObjectManager().getLoginpage();
 	}
 
-	
-	
-	
-	
+	@Given("I am on the login page")
+	public void i_am_on_the_login_page() {
+
+	}
+
+	@When("I enter my valid username and password")
+	public void i_enter_my_valid_username_and_password() {
+
+	}
+
+	@When("I click the login button")
+	public void i_click_the_login_button() {
+
+	}
+
+	@Then("I should be redirected to the home page")
+	public void i_should_be_redirected_to_the_home_page() {
+
+	}
+
+	@When("I enter an invalid username and password")
+	public void i_enter_an_invalid_username_and_password() {
+
+	}
+
+	@Then("I should see an error message indicating the login failed")
+	public void i_should_see_an_error_message_indicating_the_login_failed() {
+
+	}
+
+	@When("I leave the username or email field empty")
+	public void i_leave_the_username_or_email_field_empty() {
+
+	}
+
+	@When("I enter a valid password")
+	public void i_enter_a_valid_password() {
+
+	}
+
+	@When("I enter a valid username or email")
+	public void i_enter_a_valid_username_or_email() {
+
+	}
+
+	@When("I leave the password field empty")
+	public void i_leave_the_password_field_empty() {
+
+	}
+
+	@When("I enter an invalid email and password")
+	public void i_enter_an_invalid_email_and_password() {
+
+	}
+
+
+
+	@When("I enter whitespace characters in the username or email field")
+	public void i_enter_whitespace_characters_in_the_username_or_email_field() {
+
+	}
+
+	@When("I enter whitespace characters in the password field")
+	public void i_enter_whitespace_characters_in_the_password_field() {
+
+	}
+
+	@Then("I should")
+	public void i_should() {
+
+	}
+
+	@When("I enter my valid email and password")
+	public void i_enter_my_valid_email_and_password() {
+
+	}
 
 }
